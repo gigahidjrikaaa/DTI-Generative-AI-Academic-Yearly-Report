a8be1174afffbbf54a25c4889581f9a9bb3c6f37
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2023-12-26 23:45:46 +0700

    feat: added fonts

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -13,6 +13,23 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("OpenSans-Bold.ttf", "OpenSansBold");
+				fonts.AddFont("OpenSans-Light.ttf", "OpenSansLight");
+				fonts.AddFont("OpenSans-ExtraBold.ttf", "OpenSansExtraBold");
+				fonts.AddFont("OpenSans-Italic.ttf", "OpenSansItalic");
+				fonts.AddFont("OpenSans-LightItalic.ttf", "OpenSansLightItalic");
+				fonts.AddFont("OpenSans-SemiboldItalic.ttf", "OpenSansSemiboldItalic");
+				fonts.AddFont("OpenSans-BoldItalic.ttf", "OpenSansBoldItalic");
+				fonts.AddFont("OpenSans-ExtraBoldItalic.ttf", "OpenSansExtraBoldItalic");
+				fonts.AddFont("Roboto-Regular.ttf", "RobotoRegular");
+				fonts.AddFont("Roboto-Medium.ttf", "RobotoMedium");
+				fonts.AddFont("Roboto-Bold.ttf", "RobotoBold");
+				fonts.AddFont("Roboto-Light.ttf", "RobotoLight");
+				fonts.AddFont("Roboto-Thin.ttf", "RobotoThin");
+				fonts.AddFont("Roboto-Italic.ttf", "RobotoItalic");
+				fonts.AddFont("Roboto-MediumItalic.ttf", "RobotoMediumItalic");
+				fonts.AddFont("Roboto-BoldItalic.ttf", "RobotoBoldItalic");
+				fonts.AddFont("Roboto-LightItalic.ttf", "RobotoLightItalic");
 			});
 
 #if DEBUG
